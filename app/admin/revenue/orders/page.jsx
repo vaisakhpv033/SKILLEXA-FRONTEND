41e38e0fe795c797a2d10ccd2675cf2c35ebc5e3
Vaisakh P V <vaisakhpv033@gmail.com>
2025-04-16 11:06:37 +0530

    Fixed dynamic server usage error

--- app/admin/revenue/orders/page.jsx
+++ app/admin/revenue/orders/page.jsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import React from 'react';
 import { Separator } from '@/components/ui/separator';
 import { getAdminOrderData } from '@/lib/server/adminOrderHistory';
